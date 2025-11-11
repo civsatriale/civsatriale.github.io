85b4fcd38888deb585b9e95be3c8892b4ee739ac
civsatriale <civsatriale@gmail.com>
2025-11-10 19:49:37 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,4 @@
-function searchContent() {
-    const input = document.getElementById("searchInput").value.toLowerCase();
-    const articles = document.querySelectorAll("#constitution article");
-    
-    articles.forEach(article => {
-        const text = article.innerText.toLowerCase();
-        if (text.includes(input)) {
-            article.style.display = "block";
-        } else {
-            article.style.display = "none";
-        }
-    });
+function showSection(id) {
+    document.querySelectorAll('.section').forEach(sec => sec.classList.remove('active'));
+    document.getElementById(id).classList.add('active');
 }
